716648f0eb8ac37bb45a8e9d90408c067d51d9a6
Cameron Hudson <CameronHudson8@gmail.com>
2023-01-22 17:04:40 -0800

    fix: fix typo in endpoint name

--- variables.tf
+++ variables.tf
@@ -53,7 +53,7 @@ variable "image_acceptance_test" {
 
 variable "image_api" {
   type    = string
-  default = "us-west1-docker.pkg.dev/cameronhudson8/iskprinter/api:da527017b2bc3d2cff5a4324de382e61fc503c38"
+  default = "us-west1-docker.pkg.dev/cameronhudson8/iskprinter/api:540de8729af291e0eb4f89bfc5726bece8613a17"
 }
 
 variable "image_frontend" {
